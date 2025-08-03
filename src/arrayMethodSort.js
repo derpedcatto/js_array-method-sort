4e7275f83966a85b34b475afec4745bd1ee19aa1
Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>
2025-08-03 13:46:33 +0300

    add solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,41 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    function swapNums(arr, i, j) {
+      const temp = arr[j];
+
+      arr[j] = arr[i];
+      arr[i] = temp;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      let minValue = this[i] + '';
+
+      for (let j = i + 1; j < this.length; j++) {
+        const currentValue = this[j] + '';
+
+        if (compareFunction) {
+          const result = compareFunction(minValue, currentValue);
+
+          if (result > 0) {
+            swapNums(this, i, j);
+
+            minValue = currentValue;
+          }
+
+          continue;
+        }
+
+        if (currentValue < minValue) {
+          swapNums(this, i, j);
+
+          minValue = currentValue;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
